c9ee3861aab9a74e17412d019a07b74efd832e43
Gaurav Bohra <gaurav.bohra@mediaagility.com>
2020-04-24 13:21:30 +0530

    Update test_local.sh

--- courses/machine_learning/deepdive/06_structured/pipelines/containers/bqtocsv/test_local.sh
+++ courses/machine_learning/deepdive/06_structured/pipelines/containers/bqtocsv/test_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #bash ../build_container.sh
-
+echo "test change"
 PROJECT_ID=$(gcloud config config-helper --format "value(configuration.properties.core.project)")
 docker run -t gcr.io/${PROJECT_ID}/babyweight-pipeline-bqtocsv:latest \
        --project ${PROJECT_ID} --mode local --bucket ${PROJECT_ID}-kfpdemo
